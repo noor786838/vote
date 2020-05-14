a8a3531e4910adae3fe7c1fa718c786034bc9983
noor786838 <63953012+noor786838@users.noreply.github.com>
2020-05-14 11:23:20 +0530

    do vote
    to print if u can vote r not

---
+++ hari.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int age;
+	cout<<"enter your age"<<endl;
+	cin>>age;
+	if(age>=18)
+	{
+	cout<<"you are eligible to vote";
+}
+	if(age<18)
+	{
+	cout<<"you are not eligible to vote";
+}
+	
+}
